32399257c075f815e061ea2b4490177f55e40b86
kasunkv <kvkrusl@gmail.com>
2015-11-22 09:06:54 +0530

    Add config.json to hold the configurations and update the Startup.cs

--- src/Traveler/Startup.cs
+++ src/Traveler/Startup.cs
@@ -5,25 +5,52 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Traveler.Services;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.Runtime;
 
-namespace TheWorldVS
+namespace Traveler
 {
     public class Startup
     {
+
+        public static IConfiguration Configuration;
+
+        public Startup(IApplicationEnvironment appEnv) {
+
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, IHostingEnvironment env)
         {
+            services.AddMvc();
+
+            // Setting up dependancy Injection
+            if (env.IsDevelopment()) {
+                services.AddScoped<IMailService, DebugMailService>();
+            } else {
+                //services.AddScoped<IMailService, RealMailService>();
+                services.AddScoped<IMailService, DebugMailService>();
+            }
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
-
+            // Enable serving of static file
             app.UseStaticFiles();
-
+            // Enable MVC app
+            app.UseMvc(config => {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new  { controller = "App", action = "Index" }
+                );
+            });
         }
     }
 }
