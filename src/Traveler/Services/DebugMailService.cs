022057192a995a049a469b586551d4352e774a08
kasunkv <kvkrusl@gmail.com>
2015-11-22 09:05:25 +0530

    Added interfac for mail service

---
+++ src/Traveler/Services/DebugMailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traveler.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string body) {
+            Debug.WriteLine($"Sending Mail: To: {to}, With The Subject: {subject}, From: {from}");
+            return true;
+        }
+    }
+}
---
+++ src/Traveler/Services/IMailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traveler.Services
+{
+    public interface IMailService
+    {
+        bool SendMail(string to, string from, string subject, string body);
+    }
+}
