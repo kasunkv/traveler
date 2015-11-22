9b3ae6ff7e1d5afeb062dccff7e1da2d38ebca20
kasunkv <kvkrusl@gmail.com>
2015-11-22 16:26:34 +0530

    Added the TravelerContext Db Context file

---
+++ src/Traveler/Models/TravelerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traveler.Models
+{
+    public class TravelerContext : DbContext
+    {
+        public TravelerContext() {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            var connectionString = Startup.Configuration["Data:TravelerContextConnection"];
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
